7e56f8683d309af1071581f8e70a8588eb1d941f
Vitaly Grinberg <vitus133@yahoo.com>
2018-06-03 08:47:22 +0300

    Added logger name to trace

--- infrastructure.py
+++ infrastructure.py
@@ -7,9 +7,9 @@ import os
 
 def get_logger():
     LOG_FILENAME = 'healer.log'
-    logger = logging.getLogger('vbs healer')
+    logger = logging.getLogger('vbs.healer')
     logger.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s [%(filename)s:%(lineno)s]: %(message)s',
+    formatter = logging.Formatter('%(name)s %(asctime)s %(levelname)s [%(filename)s:%(lineno)s]: %(message)s',
                                 datefmt='%Y-%m-%d %H:%M:%S')
     
     # logging to file
